998b5d056d96610cc85a0a91cc71194e157b0379
Siddharth <siddhu1419@gmail.com>
2023-08-08 17:46:05 +0530

    intial setup

---
+++ App.js
@@ -0,0 +1,27 @@
+const heading = [React.createElement(
+    "div",
+     {id: "parent-header"} , 
+     [React.createElement(
+        "h1",
+         {id: "heading"} , 
+         "hello siddharth"),
+         React.createElement(
+            "h1",
+             {id: "heading"} , 
+             "hello heading2"),
+        ]),
+        React.createElement(
+            "div",
+             {id: "child-header"} , 
+             [React.createElement(
+                "h1",
+                 {id: "heading"} , 
+                 "hello siddharth"),
+                 React.createElement(
+                    "h1",
+                     {id: "heading"} , 
+                     "hello heading2"),
+                ]),
+    ];
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
