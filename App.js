856d9aff6cada3a314c2a2355b73b48a4fe40811
Siddharth <siddhu1419@gmail.com>
2023-08-10 20:44:27 +0530

    React basics

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-const heading = [React.createElement(
-    "div",
-     {id: "parent-header"} , 
-     [React.createElement(
-        "h1",
-         {id: "heading"} , 
-         "hello siddharth"),
-         React.createElement(
-            "h1",
-             {id: "heading"} ,  
-             "hello heading2"),
-        ]),
-        React.createElement(
-            "div",
-             {id: "child-header"} , 
-             [React.createElement(
-                "h1",
-                 {id: "heading"} , 
-                 "hello siddharth"),
-                 React.createElement(
-                    "h1",
-                     {id: "heading"} , 
-                     "hello heading2"),
-                ]),
-    ];
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const heading = [React.createElement(
+//    "div",
+//    { id: "parent-header" },
+//    [React.createElement(
+//       "h1",
+//       { id: "heading" },
+//       "hello siddharth"),
+//    React.createElement(
+//       "h1",
+//       { id: "heading" },
+//       "hello heading2"),
+//    ]),
+// React.createElement(
+//    "div",
+//    { id: "child-header" },
+//    [React.createElement(
+//       "h1",
+//       { id: "heading" },
+//       "hello siddharth"),
+//    React.createElement(
+//       "h1",
+//       { id: "heading" },
+//       "hello heading2"),
+//    ]),
+// ];
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const Header = () =>  <h1 className="header">hello world</h1>
+
+const number = [1000,100,200,300]
+
+const Heading2 = () => (
+   <div>
+      <Header />
+      {(number).map(n=> (n / 100)).join(',')}
+      <h2 style={{color: "red"}}>hello see up</h2>
+   </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<Heading2 />);
